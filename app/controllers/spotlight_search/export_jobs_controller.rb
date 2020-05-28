903560eb61ff773b4aa1cff05707f04195df23a0
Camilo Forero <camilo.forero3@googlemail.com>
2020-05-28 17:34:35 +0530

    fix: unsafe params are now allowed

--- app/controllers/spotlight_search/export_jobs_controller.rb
+++ app/controllers/spotlight_search/export_jobs_controller.rb
@@ -1,19 +1,17 @@
 module SpotlightSearch
   class ExportJobsController < ApplicationController
     def export_job
-      begin
-        klass = params[:class_name].constantize
-        if klass.validate_exportable_columns(params[:columns])
-          ExportJob.perform_later(klass.name, params[:email], params[:columns], params[:filters], params[:sort])
-          notice = 'Successfully queued for export'
-        else
-          notice = 'Invalid columns found'
-        end
-      rescue StandardError => e
-        notice = e.message
-      ensure
-        redirect_back fallback_location: root_path, notice: notice
+      klass = params[:class_name].constantize
+      if klass.validate_exportable_columns(params[:columns])
+        ExportJob.perform_later(klass.name, params[:email], params[:columns], params[:filters].to_unsafe_h, params[:sort].to_unsafe_h)
+        notice = 'Successfully queued for export'
+      else
+        notice = 'Invalid columns found'
       end
+    rescue StandardError => e
+      notice = e.message
+    ensure
+      redirect_back fallback_location: root_path, notice: notice
     end
   end
 end
