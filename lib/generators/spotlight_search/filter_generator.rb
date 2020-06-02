bff2c506651b0a04d26ad5aa2072400446a00167
Anbazhagan <anbublacky@gmail.com>
2020-06-02 21:19:05 +0530

    fix: renamed .template to respective file format, added few install files that are needed, filter generator changes, controller added

--- lib/generators/spotlight_search/filter_generator.rb
+++ lib/generators/spotlight_search/filter_generator.rb
@@ -4,32 +4,12 @@ module SpotlightSearch
       source_root File.expand_path('templates', __dir__)
 
       argument :model,                type: :string, required: true, desc: "Pass a model name"
-      class_option :init_js,          type: :boolean, default: false, desc: "Pass true if you're gonna export input filters"
       class_option :filters,          aliases: "-f", type: :array, desc: "Pass filters and scopes as an array. E.x: date_filter:datetime search:input"
-
-      def initialize_js_packages
-        if @options.init_js?
-          system("yarn add jquery")
-          system("yarn add coffeescript")
-          system("yarn add select2")
-          inject_into_file 'app/assets/stylesheets/application.css', before: " */" do
-            " *= require select2\n"
-          end        
-          unless File.directory?("app/javascript/application/coffee_scripts")
-            Dir.mkdir("app/javascript/application")
-            Dir.mkdir("app/javascript/application/coffee_scripts")
-          end
-          template "application.js.template", "app/javascript/packs/application.js"
-          template 'scaffolds.coffee.template', "app/javascript/application/coffee_scripts/scaffolds.coffee"        
-        end
-      end
 
       def copy_filter_contents_to_app
         if @options.filters?
-          unless File.directory?("app/views/#{model}")
-            Dir.mkdir("app/views/#{model}")
-          end
-          template 'filters.html.erb.template', "app/views/#{model}/_filters.html.slim"
+          template 'filters.html.erb', "app/views/admin/#{model}/_filters.html.slim"
+          template 'controller.rb.erb', "app/controllers/admin/#{model}_controller.rb"
         end
       end
 
--- lib/generators/spotlight_search/install_generator.rb
+++ lib/generators/spotlight_search/install_generator.rb
@@ -7,24 +7,32 @@ module SpotlightSearch
 
       def copy_webpacker_gem_assets
         copy_file 'webpacker_gem_assets.rb', 'config/initializers/webpacker_gem_assets.rb'
+        copy_file 'webpacker.yml', 'config/webpacker.yml'
+        copy_file 'environment.js', 'config/webpack/environment.js'
+        copy_file 'coffee.js', 'config/webpack/loaders/coffee.js'
       end
 
       def add_essentials
-        append_to_file "app/javascript/packs/application.js" do 
-          "require ('spotlight_search')"
+        system("yarn add jquery")
+        system("yarn add coffeescript")
+        system("yarn add select2")
+        inject_into_file 'app/assets/stylesheets/application.css.scss', before: " */" do
+          " *= require select2\n"
         end
-        gem 'kaminari', '~> 1.2.1'
+        template "application.js", "app/javascript/packs/application.js"
+        template 'scaffolds.coffee', "app/javascript/application/coffee_scripts/scaffolds.coffee"
+        gem 'kaminari', '~> 1.2.1' unless File.readlines("Gemfile").grep(/kaminari/).size > 0
       end
-  
+
       def edit_webpacker_yml
         webpacker = YAML::load_file('config/webpacker.yml')
         webpacker['default']['resolved_gems_output_path'] = '/tmp/_add_gem_paths.js'
         webpacker['default']['resolved_gems'] = ['spotlight_search']
         webpacker['default']['extensions'] = '.coffee'
         webpacker['production']['webpack_compile_output'] = true
-        File.open('config/webpacker.yml', 'w') {|f| f.write webpacker.to_yaml }       
+        File.open('config/webpacker.yml', 'w') {|f| f.write webpacker.to_yaml }
       end
-    
+
     end
   end
 end
